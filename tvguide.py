d41c43c72ced2f328c7964146e65a8f7be2960bc
Crinibus <57172157+Crinibus@users.noreply.github.com>
2021-01-08 21:16:45 +0100

    Update tvguide.py to use Tv2's API instead of ther website
    - add validate_arguments.py to validate (so far) argument --day

--- tvguide.py
+++ tvguide.py
@@ -1,7 +1,8 @@
 import requests
-from bs4 import BeautifulSoup
+import json
 import argparse
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
+import validate_arguments as validate_args
 
 
 def argparse_setup():
@@ -12,7 +13,8 @@ def argparse_setup():
         '-c',
         '--channel',
         help='the channel with the user want to see programs from',
-        action='append'
+        action='append',
+        type=str
     )
 
     parser.add_argument(
@@ -29,80 +31,104 @@ def argparse_setup():
         action='store_true'
     )
 
+    parser.add_argument(
+        '-d',
+        '--day',
+        help='the date/day the programs is running, default today (0)',
+        type=validate_args.validate_date_argument,
+        default=0
+    )
+
+    parser.add_argument(
+        '--category',
+        help='only show the programs with the chosen category(s)',
+        action='append',
+        type=str
+    )
+
     return parser.parse_args()
 
 
-def get_soup(URL='https://tvtid.tv2.dk/'):
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"}
-    cookies = dict(cookies_are='working')
-    response = requests.get(URL, headers=headers, cookies=cookies)
-    return BeautifulSoup(response.text, 'html.parser')
+def get_specified_date(relative_date: int):
+    date = datetime.today()
+    date += timedelta(days=relative_date)
+    date = date.strftime('%Y-%m-%d')
+    return date
+
+
+def get_data():
+    date = get_specified_date(args.day)
+    response = requests.get(f'https://tvtid-api.api.tv2.dk/api/tvtid/v1/epg/dayviews/{date}?ch=1&ch=2&ch=3&ch=4&ch=5&ch=6&ch=7&ch=8&ch=14&ch=15&ch=31&ch=37&ch=70&ch=71&ch=77&ch=93&ch=94&ch=118&ch=133&ch=145&ch=153&ch=156&ch=157&ch=185&ch=219&ch=248&ch=10066&ch=10089&ch=10093&ch=10111&ch=10153&ch=10154&ch=10155&ch=12566&ch=12948&ch=15049&ch=2147483561')
+
+    return response.json()
 
 
 def get_channels() -> list:
     """Find all channels with programs, and return the list"""
-    soup = get_soup()
-    channels = soup.find_all('article', class_='tv2epg-channel')
-    return channels
+    data = get_data()
+    #channels = [channel for channel in data]
+    return data
 
 
-def get_programs(channel_list: list, channels: list) -> dict:
+def get_programs(channel_list: list, user_channels: list) -> dict:
     """Find the programs for the specified channels and return a dict with them where the channels are the keys"""
     channel_index = {
-        'dr1': 0,
-        'tv2': 1,
-        'tv3': 2,
-        'dr2': 3,
-        'tv2-charlie': 4,
-        'tv2-news': 5,
-        'kanal-5': 6,
-        'tv3-plus': 7,
-        'tv2-zulu': 8,
-        'dr-ramasjang': 9,
-        'kanal-4': 10,
-        'tv2-sport': 11,
-        'tv2-sport-x': 12,
-        'tv3-sport': 13,
-        'tv3-puls': 14,
-        '6eren': 15,
-        'disney-channel': 16,
-        'tv2-fri': 17,
-        'canal-9': 18,
-        'discovery-channel': 19,
-        'tlc': 20,
-        'nickelodeon': 21,
-        'national-geographic-channel': 22,
-        'tv3-max': 23,
-        'cartoon': 24,
-        'disney-junior': 25,
-        'dk4': 26,
-        'mtv': 27,
-        'animal-planet': 28,
-        'investigation-discovery': 29,
-        'vh1': 30,
-        'eurosport-2': 31,
-        'boomerang': 32,
+        'dr1': 1,
+        'tv2': 3,
+        'tv3': 5,
+        'dr2': 2,
+        'tv2-charlie': 31,
+        'tv2-news': 133,
+        'kanal-5': 7,
+        'tv3-plus': 6,
+        'tv2-zulu': 4,
+        'dr-ramasjang': 10153,
+        'kanal-4': 8,
+        'tv2-sport': 77,
+        'tv2-sport-x': 2147483561,
+        'tv3-sport': 156,
+        'tv3-puls': 10093,
+        '6eren': 10066,
+        'disney-channel': 14,
+        'tv2-fri': 12566,
+        'canal-9': 10111,
+        'discovery-channel': 70,
+        'tlc': 118,
+        'nickelodeon': 153,
+        'national-geographic-channel': 94,
+        'tv3-max': 12948,
+        'cartoon': 185,
+        'disney-junior': 157,
+        'dk4': 15,
+        'mtv': 71,
+        'animal-planet': 93,
+        'investigation-discovery': 15049,
+        'vh1': 219,
+        'eurosport-2': 37,
+        'boomerang': 248,
     }
 
     program_dict = {}
 
     allChannelsChosen = False
 
-    if channels[0] == 'all':
+    if user_channels[0] == 'all':
         allChannelsChosen = True
 
     if not allChannelsChosen:
-        for channel in channels:
-            channel = channel.lower()
+        for user_channel in user_channels:
+            user_channel = user_channel.lower()
 
             # Check if channel is in channel_indix dict, if not add channel as key
-            if channel not in program_dict.keys():
-                program_dict.update({channel: []})
+            if user_channel not in program_dict.keys():
+                program_dict.update({user_channel: []})
 
             # Get index from dict
-            index = channel_index[channel]
-            # Add all program in channel to program_dict
-            program_dict[channel].append(channel_list[index].find_all('li', class_='tv2epg-program-item'))
+            index = channel_index[user_channel]
+
+            for channel in channel_list:
+                if channel['id'] == str(index):
+                    program_dict[user_channel].append(channel["programs"])
     else:
         for channel in channel_index.keys():
             # Check if channel is in channel_indix dict, if not add channel as key
@@ -111,10 +137,28 @@ def get_programs(channel_list: list, channels: list) -> dict:
 
             # Get index from dict
             index = channel_index[channel]
-            # Add all program in channel to program_dict
-            program_dict[channel].append(channel_list[index].find_all('li', class_='tv2epg-program-item'))
 
+            for channel in channel_list:
+                program_dict[channel].append(channel["programs"])
+    
     return program_dict
+
+
+def convert_unix_time(unix_time: int, toShow: bool):
+    """Convert UNIX times to hour and minutes to an int, e.g.: 1604692800 -> 2000\n
+       Times are shifted from UTC to UTC+1 (CET)"""
+    
+    # TODO: Use datetime timezone to convert to my timezone
+
+    time = datetime.utcfromtimestamp(unix_time)
+    time += timedelta(hours=1)
+    
+    if toShow:
+        time = time.strftime('%H:%M')
+    else:
+        time = int(time.strftime('%H%M'))
+
+    return time
 
 
 def print_all_programs(program_dict: dict):
@@ -124,9 +168,11 @@ def print_all_programs(program_dict: dict):
         print(f'\n{channel.upper().replace("-", " ")}')
         for program_list in program_dict[channel]:
             for program in program_list:
-                timeStart = program.time.text
-                progsTitle = program.strong.text
+                timeStart = int(program['start'])
+                timeStart = convert_unix_time(timeStart, toShow=True)
+                progsTitle = program['title']
                 print(f"{timeStart} > {progsTitle}")
+    print()
 
 
 def print_time_program(program_dict: dict, timeStart: str):
@@ -143,31 +189,27 @@ def print_time_program(program_dict: dict, timeStart: str):
         for program_list in program_dict[channel]:
             for program in program_list:
                 # Get time start end end of program in UNIX time from HTML
-                time_data_unix_start = int(program.get('data-start'))
-                time_data_unix_end = int(program.get('data-stop'))
-                # Convert UNIX times to hour and minutes to an int, e.g.: 1604692800 -> 2000
-                # Times are shifted from UTC to UTC+1 (CET)
+                time_data_unix_start = int(program['start'])
+                time_data_unix_end = int(program['stop'])
 
-                # TODO: Use datetime timezone to convert to my timezone
-                time_data_unix_start_convert = int(datetime.utcfromtimestamp(time_data_unix_start + 3600).strftime('%H%M'))
-                time_data_unix_end_convert = int(datetime.utcfromtimestamp(time_data_unix_end + 3600).strftime('%H%M'))
+                time_start = convert_unix_time(time_data_unix_start, toShow=False)
+                time_end = convert_unix_time(time_data_unix_end, toShow=False)
 
                 # Append the program that start at the specified time to dict progsTime
-                if program.time.text == timeStart:
+                if time_start == int(timeStart.replace(':', '')):
                     progsTime[channel].append(program)
                 # Append the program that is running at the specified time to dict progsTime
-                elif time_data_unix_start_convert < int(timeStart.replace(':', '')) and time_data_unix_end_convert > int(timeStart.replace(':', '')):
+                elif time_start < int(timeStart.replace(':', '')) and time_end > int(timeStart.replace(':', '')):
                     progsTime[channel].append(program)
 
     for channel in progsTime.keys():
         if len(progsTime[channel]) > 0:
-            # timeEnd = progsAfter[channel][0].time.text
-            progsTitle = progsTime[channel][0].get('title')
-            timeStart_unix = int(progsTime[channel][0].get('data-start'))
-            timeEnd_unix = int(progsTime[channel][0].get('data-stop'))
+            progsTitle = progsTime[channel][0]['title']
+            timeStart_unix = int(progsTime[channel][0]['start'])
+            timeEnd_unix = int(progsTime[channel][0]['stop'])
             # Times are shifted from UTC to UTC+1 (CET)
-            timeStart = datetime.utcfromtimestamp(timeStart_unix + 3600).strftime('%H:%M')
-            timeEnd = datetime.utcfromtimestamp(timeEnd_unix + 3600).strftime('%H:%M')
+            timeStart = convert_unix_time(timeStart_unix, toShow=True)
+            timeEnd = convert_unix_time(timeEnd_unix, toShow=True)
 
             print(f'{channel.upper().replace("-", " ")}')
             print(f'{timeStart} - {timeEnd} > {progsTitle}\n')
@@ -180,7 +222,6 @@ def main(args):
     if not args.channel:
         print('No channels chosen: using default channels (dr1, tv2)')
         args.channel = ['dr1', 'tv2']
-        # return
 
     channel_list = get_channels()
     program_dict = get_programs(channel_list, args.channel)
