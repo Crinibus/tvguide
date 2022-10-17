870d8a8f94d51bc0fed50755e58079a4bf6fa85f
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-10-17 21:33:26 +0200

    Create main_v2.py to test with

---
+++ main_v2.py
@@ -0,0 +1,19 @@
+import tvguide as tv
+
+
+def main():
+    args = tv.argparse_setup()
+
+    tvguide = tv.TvGuide()
+
+    tvguide.parse_arguments(args)
+
+    api_data = tv.ApiManager.get_data(tvguide.relative_date)
+
+    tvguide.parse_api_data(api_data)
+
+    tvguide.print_programs()
+
+
+if __name__ == "__main__":
+    main()
